a0ccdb5082919926fab8ba7157b646cab4b8ed6b
Sana Khemir <archi.khemirsana@gmail.com>
2025-01-13 10:52:20 +0100

    changement

---
+++ index.js
@@ -0,0 +1,13 @@
+// Structures de données que je ne maîtrise pas encore :
+      //Tas binaire: la logique d'insertion et de suppression est encore floue.
+           //je cherche à mieux comprendre la manière dont les éléments sont organisés et extraits.
+
+      //Tables de hachage: les collisions et les différentes méthodes de résolution (chaînage, sondage linéaire, etc.) ne sont pas encore claires.
+           // je cherche à mieux comprendre la gestion des collisions.
+
+//   Algorithmes de tri que je ne maîtrise pas encore :
+     //Tableau de suffixes: Je trouve difficile de comprendre comment construire le tableau de suffixes à partir d'une chaîne donnée.
+          //je cherche à mieux comprendre étape par étape comment générer un tableau de suffixes à partir d’une chaîne.
+
+     //Arbre de suffixes: l'idée d'un arbre qui contient tous les suffixes possibles d'une chaîne me semble complexe à comprendre
+         //je vais faire des exercices pratiques où je construis un arbre de suffixes à partir de petites chaînes pour mieux appréhender le processus
